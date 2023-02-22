a2a3372f1af526196547938d2f9e28f72e3ebf01
VinceIngram07 <102874433+VinceIngram07@users.noreply.github.com>
2023-02-22 16:25:10 -0500

    commit 2

--- Run/CsvTransfer.js
+++ Run/CsvTransfer.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 // set the input and output file paths
-// Change Input and Output and  File names
+// Change inputFilePath value!
 const inputFilePath = 'organizations-100.csv';
-const outputFilePath = 'organizations2-100.csv';
+const outputFilePath = 'outputData.csv';
 
 // set the columns to select from the input file
-// Change Column array to desired names
-const selectedColumns = ['Organization Id', 'Name', 'Website'];
+// Change selectedColumn array to desired names!
+const selectedColumns = ['Index', 'Organization Id', 'Name', 'Website'];
 
 // create an array to hold the selected data
 const selectedData = [];
@@ -24,13 +24,14 @@ fs.createReadStream(inputFilePath)
 		selectedData.push(selectedRow);
 	})
 	.on('end', () => {
-		// write the selected data to the output CSV file
+
+		// write the selected data to the output CSV file 
 		const csvWriter = require('csv-writer').createObjectCsvWriter({
 			path: outputFilePath,
 			header: selectedColumns.map((col) => ({ id: col, title: col })),
 		});
 		csvWriter.writeRecords(selectedData)
 			.then(() => {
-				console.log('Selected data written to output file.');
+				console.log('Selected data columns written to output file.');
 			});
 	});
